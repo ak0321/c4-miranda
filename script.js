21812da1fb2e7ed33b1df445723ae091c4198b1d
Aman Kosariya <aman6264095131@gmail.com>
2024-08-17 22:26:37 +0530

    no changes yet

---
+++ script.js
@@ -0,0 +1,26 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+var tl = gsap.timeline();
+tl.to(".page1", {
+    y:"100vh",
+    scale:0.5,
+    duration:0,
+
+})
+
+tl.to(".page1", {
+  y: "30vh",
+  duration: .5,
+  delay: 1,
+  
+});
+
+tl.to(".page1",{
+    y: "0vh",
+    rotate:0,
+    scale:1,
+    duration:.5
+})
